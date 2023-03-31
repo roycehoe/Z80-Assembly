34aba7360361503cf61334c264888c1d32b34246
roycehoe <royceinbox@hotmail.com>
2023-04-01 06:46:22 +0800

    feat: Create mapping between gameboy mem location names and memory locations

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
+from enum import Enum, auto
 from functools import partial
 from io import DEFAULT_BUFFER_SIZE
 from pathlib import Path
 from typing import Any
 
 from data import CHAR_ENCODING
+
+
+class GameboyMemLocation(Enum):
+    ROM0 = auto()
+    ROM3 = auto()
+    VRA0 = auto()
+    SRA1 = auto()
+    WRA0 = auto()
+    WRA1 = auto()
+    ECH0 = auto()
+    ECH1 = auto()
+    OAM = auto()
+    UNLABELED = auto()
+    IO = auto()
+    HRAM = auto()
+
+
+GAMEBOY_MEM_LOCATION_TO_RANGE_MAP: dict[GameboyMemLocation, range] = {
+    GameboyMemLocation.ROM0: range(0, 0x4000),
+    GameboyMemLocation.ROM3: range(0x4000, 0x8000),
+    GameboyMemLocation.VRA0: range(0x8000, 0xA000),
+    GameboyMemLocation.SRA1: range(0xA000, 0xC000),
+    GameboyMemLocation.WRA0: range(0xC000, 0xD000),
+    GameboyMemLocation.WRA1: range(0xD000, 0xE000),
+    GameboyMemLocation.ECH0: range(0xE000, 0xF000),
+    GameboyMemLocation.ECH1: range(0xF000, 0xFE00),
+    GameboyMemLocation.OAM: range(0xFE00, 0xFEA0),
+    GameboyMemLocation.UNLABELED: range(0xFEA0, 0xFF00),
+    GameboyMemLocation.IO: range(0xFF00, 0xFF80),
+    GameboyMemLocation.HRAM: range(0xFF80, 0xFFFF),
+}
+
+
+def get_gameboy_mem_location(hex: int):
+    ...
 
 
 def file_byte_iterator(path):
