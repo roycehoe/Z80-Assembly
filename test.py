da5b43e123436eef1ec1c9b4bbbe7d5d383b46ee
roycehoe <royceinbox@hotmail.com>
2023-03-31 11:16:47 +0800

    feat: Create pokemon parser

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+import json
 from pathlib import Path
 from functools import partial
 from io import DEFAULT_BUFFER_SIZE
@@ -7,17 +8,42 @@ from io import DEFAULT_BUFFER_SIZE
 class InvalidPokemonLevel(Exception):
     pass
 
+
+
+
 @dataclass
 class Pokemon:
-    index: int
-    pokedex_no: str
-    name: str
-    type_1: str
-    type_2: str
+    hex: str = field(init=False)
+    index: int = field(metadata={"alias": 'Index'})
+    pokedex_no: str = field(metadata={"alias": 'Pokedex'})
+    name: str = field(metadata={"alias": 'Pokemon'})
+    type_1: str = field(metadata={"alias": 'Type 1'})
+    type_2: str = field(metadata={"alias": 'Type 2'})
+
+    def __post_init__(self):
+        self.hex = hex(self.index)
+
 
 @dataclass
 class Pokedex:
     pokemon: list[Pokemon]
+
+
+def get_pokedex():
+    path = Path('./pokedex.json')
+    pokemons = []
+    with path.open('r') as file:
+        pokemon_file = json.load(file)
+        for pokemon in pokemon_file:
+            del pokemon["Hex"] # Hex in json is in string. Prefer to calculate this in Pokemon class
+            pokemons.append(Pokemon(**pokemon))
+    return Pokedex(pokemon=pokemons)
+
+print(get_pokedex())
+
+
+
+
 
 
 def file_byte_iterator(path = './Pokemon_red.dump'):
